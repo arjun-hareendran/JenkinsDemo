8db355fbe0ebe1a0eaa496255159e7b2aad800ec
Hareendran <ahareen@apac.corpdir.net>
2018-05-25 09:07:51 +0530

    reporting2

--- src/test/java/MathamaticalTest.java
+++ src/test/java/MathamaticalTest.java
@@ -6,10 +6,18 @@ public class MathamaticalTest {
 
 	@Test
 	public void test() {
-		
-		Mathamatical obj =new Mathamatical();
+
+		Mathamatical obj = new Mathamatical();
 		assertTrue(obj.add(2, 2) == 4);
-		
+
+	}
+
+	@Test
+	public void test2() {
+
+		Mathamatical obj = new Mathamatical();
+		assertTrue(obj.add(4, 4) == 8);
+
 	}
 
 }
