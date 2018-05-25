a4625d12ee16e548e9f480f22689592b5748d5ce
Hareendran <ahareen@apac.corpdir.net>
2018-05-25 08:49:08 +0530

    Initial commit

---
+++ src/main/java/Mathamatical.java
@@ -0,0 +1,9 @@
+
+public class Mathamatical {
+
+	
+	public int add(int a,int b) {
+		
+		return a+b;
+	}
+}
---
+++ src/test/java/MathamaticalTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathamaticalTest {
+
+	@Test
+	public void test() {
+		
+		Mathamatical obj =new Mathamatical();
+		assertTrue(obj.add(2, 2) == 4);
+		
+	}
+
+}
